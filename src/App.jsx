0bfffc3f65009d45679fcdaf3ac723fb14c97a7a
Marc Grupe <marc.grupe@gmail.com>
2025-04-01 13:32:09 -0400

    completed first react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import './App.css'
+import { puppyList } from "./data"
+import { useState } from "react"
+import './app.css'
+import './index.css'
 
 function App() {
-  
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
 
   return (
     <>
-      <div>
+      <div className="App">
+        <div>
+          {featPupId && (
+            <div className="card">
+              <h2>{featuredPup.name}</h2>
+              <ul>
+                <li>Age: {featuredPup.age}</li>
+                <li>Email: {featuredPup.email}</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        {puppies &&
+          puppies.map((puppy) => {
+            return (
+              <p
+                onClick={() => {
+                  setFeatPupId(puppy.id);
+                }}
+                key={puppy.id}
+              >
+                {puppy.name}
+              </p>
+            );
+          })}
       </div>
     </>
-  )
+  );
 }
 
 export default App
